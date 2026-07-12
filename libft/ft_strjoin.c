8e9fb6ccb2c264ca22d71375a4e6aacae7ba5f1c
Judith <89136088+Yuudith@users.noreply.github.com>
2026-07-12 11:47:52 +0200

    Create ft_strjoin.c

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	size_t	i;
+	size_t	j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	joined = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (joined == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	joined[i + j] = '\0';
+	return (joined);
+}
