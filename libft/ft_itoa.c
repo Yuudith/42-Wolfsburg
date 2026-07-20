af55d05d0e9808ce0c61cfa4a070ed85e377b0e7
Judith <89136088+Yuudith@users.noreply.github.com>
2026-07-20 15:54:59 -0400

    Create ft_itoa.c

---
+++ libft/ft_itoa.c
@@ -0,0 +1,46 @@
+
+
+#include "libft.h"
+
+static size_t	ft_num_len(long n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nb;
+	size_t	len;
+
+	nb = n;
+	len = ft_num_len(nb);
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	if (nb == 0)
+		str[0] = '0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (nb > 0)
+	{
+		str[len - 1] = (nb % 10) + '0';
+		nb = nb / 10;
+		len--;
+	}
+	return (str);
+}
