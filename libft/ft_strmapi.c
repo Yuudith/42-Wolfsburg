5cee6877f50427b7ab80633fc16b99cac5f5a1a3
Judith <89136088+Yuudith@users.noreply.github.com>
2026-07-21 19:47:27 +0200

    Create ft_strmapi.c

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,22 @@
+
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*result;
+	size_t	i;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	result = malloc(ft_strlen(s) + 1);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		result[i] = f((unsigned int)i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
