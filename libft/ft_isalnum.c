f8c447b87f25d20ca84e6428041f3fa9b00919c9
Judith Tripp <jtripp@c1r12s4.42wolfsburg.de>
2026-08-04 17:47:36 +0200

    Second final, one issue left

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: jtripp <jtripp@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/02 14:22:15 by jtripp            #+#    #+#             */
-/*   Updated: 2026/08/04 16:37:32 by jtripp           ###   ########.fr       */
+/*   Updated: 2026/08/04 17:33:04 by jtripp           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_isalnum(int c)
 {
-	if ((c >= 48 && c >= 57) || (c >= 65 && c >= 90) || (c >= 97 && c <= 122))
+	if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
@@ -23,16 +23,3 @@ int	ft_isalnum(int c)
 		return (0);
 	}
 }
-
-/*# include <stdio.h>
-
-int	main(void)
-{
-   int c;
-   c = 3;
-
-   int ft_isalnum ();
-
-   printf("%d is alphanumeric.\n", c);
-   printf("%d is not alphanumeric\n", c);
-}*/
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jtripp <jtripp@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 15:13:23 by jtripp            #+#    #+#             */
-/*   Updated: 2026/08/02 15:56:06 by jtripp           ###   ########.fr       */
+/*   Updated: 2026/08/04 17:39:31 by jtripp           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && s1[i] == s2[i])
+	while (i < n && (s1[i] || s2[i]))
 	{
-		if (s1[i] == '\0')
-			return (0);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }
